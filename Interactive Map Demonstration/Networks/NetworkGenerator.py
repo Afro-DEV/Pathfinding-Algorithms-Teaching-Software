8ddb65bb5e26b6a2a9c4f67e5b1f9ea9fb7140b1
Ibi <ibitayosalawu@gmail.com>
2025-01-28 20:16:10 +0000

    Added Network Generator for new york network

---
+++ Interactive Map Demonstration/Networks/NetworkGenerator.py
@@ -0,0 +1,24 @@
+import osmnx as ox
+import matplotlib.pyplot as plt
+import time
+class NetworkFileGenerator():
+    def __init__(self):
+        ...
+    
+    def GenerateNewYorkNetwork(self):
+        point = (40.7341874, -73.9881721)  # Center coordinate
+        graph = ox.graph.graph_from_point(point, dist=3000, network_type="drive") # While Testing
+        ox.save_graphml(graph, filepath="Interactive Map Demonstration/Networks/NewYorkNetwork.graphml")
+        
+        
+
+#Run once to generate Network Files
+if __name__ == "__main__":
+    #Establishing where Network Cache should go 
+    ox.settings.cache_folder = "Interactive Map Demonstration/Networks/Network Cache"
+    networkFileGenerator = NetworkFileGenerator()
+    
+    graph = ox.load_graphml(filepath="Interactive Map Demonstration/Networks/NewYorkNetwork.graphml")
+    fig, ax = ox.plot_graph(graph, node_size=0, edge_color='w', bgcolor='k', show=False)
+    plt.show()
+    
