f27a1fddf0853c7bafd6d60b19b276793c6c8445
Ibi <ibitayosalawu@gmail.com>
2025-02-10 19:40:04 +0000

    Moved network generator to main solution folder

--- NetworkGenerator.py
+++ NetworkGenerator.py
@@ -3,16 +3,15 @@ import matplotlib.pyplot as plt
 import time
 class NetworkFileGenerator():
     def __init__(self):
-        ...
+        self.CENTRE_POINTS = {}
     
-    @staticmethod
-    def GenerateNewYorkNetwork():
+    def GenerateNewYorkNetwork(self):
         centrePoint = (40.7341874, -73.9881721)  # Center coordinate
         graph = ox.graph.graph_from_point(centrePoint, dist=3000, network_type="drive") # While Testing
         ox.save_graphml(graph, filepath="Networks/NewYorkNetwork.graphml")
     
-    @staticmethod
-    def GenerateParisNetwork():
+    
+    def GenerateParisNetwork(self):
         #centrePoint = (35.6594347, 139.6799793)
         centrePoint = (48.8584, 2.2945)
         graph = ox.graph.graph_from_point(centrePoint, dist=3000, network_type='drive')
