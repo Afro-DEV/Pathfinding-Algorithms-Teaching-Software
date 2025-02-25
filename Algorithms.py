2ffed468004a4b201637e51434603597d793c400
Ibi <ibitayosalawu@gmail.com>
2025-02-25 15:39:29 +0000

    Move algorithms to algorithms file

--- Algorithms.py
+++ Algorithms.py
@@ -1,5 +1,9 @@
 import time
 import random
+import networkx as nx
+import math
+from Utilities import cos, sin, ConvertDegreesToRadians
+from DataStructures import MinHeap, LinkedListNode, Stack
 
 def GenerateMatrix(numberOfNodes: int,densityVal: float) -> list[list[int]]:
     '''
@@ -20,6 +24,163 @@ def GenerateMatrix(numberOfNodes: int,densityVal: float) -> list[list[int]]:
 def OutputMatrix(matrix):
     for each in matrix:
         print(each)
+
+def HaversineDistance(graph: nx.MultiDiGraph, node1: int,  node2: int) -> float:
+    '''Estimate the distance in Kilometres between 2 points on Earth's surface '''
+    coordinateNode1 = NodeToCoordinate(graph,node1)
+    coordinateNode2 = NodeToCoordinate(graph, node2)
+
+    #Get lattitude and longitude of nodes
+    lat1 = coordinateNode1[0]
+    lon1 = coordinateNode1[1]
+    lat2 = coordinateNode2[0]
+    lon2 = coordinateNode2[1]
+
+    lat1,lon1, lat2, lon2 = map(ConvertDegreesToRadians, [lat1,lon1,lat2,lon2]) # Convert degrees lattitude and longitude into radians
+    
+    # Apply Haversine formula
+    distanceLat = lat2 - lat1
+    distanceLon = lon2 - lon1
+    a = sin(distanceLat / 2)**2 + cos(lat1) * cos(lat2) * sin(distanceLon / 2)**2
+    c = 2 * math.asin(math.sqrt(a))
+    
+    # Radius of Earth in kilometres
+    RADIUS_EARTH = 6371.0
+    
+    distance = c * RADIUS_EARTH
+    return distance
+
+def EuclideanDistance(graph, node1, node2):
+    '''Calculate the aboslute distance between 2 nodes lattitude and longitude'''
+    coordinateNode1 = NodeToCoordinate(graph,node1)
+    coordinateNode2 = NodeToCoordinate(graph, node2)
+    distanceLatitude = abs(coordinateNode1[0]-coordinateNode2[0])
+    distanceLongitude = abs(coordinateNode1[1] - coordinateNode2[1])
+    distance = math.sqrt(distanceLatitude**2 + distanceLongitude**2)
+    return distance
+
+def NodeToCoordinate(graph, node) -> tuple[float,float]:
+    return graph.nodes[node]['x'], graph.nodes[node]['y']
+
+
+
+def GetPath(cameFrom, currentNode, startNode) -> list:
+    path = []
+    while currentNode in cameFrom:
+        path.append(currentNode)
+        currentNode = cameFrom[currentNode]
+    path.append(startNode)
+    return path[::-1] #Return reversed path
+
+def GetPathLinkedList(endListNode: LinkedListNode) -> list:
+    stack = Stack()
+    path = []
+    node: LinkedListNode = endListNode  # Start from the last node which is the target node
+    while node:
+        stack.Push(node.GetData())  # Add the node's value to stack
+        node = node.GetParent()  # Move backwards using the reference
+
+    while not stack.IsEmptyStack():
+        path.append(stack.Pop())
+    
+    return path 
+
+
+def AStar(graph: nx.MultiDiGraph, startNode: int, endNode: int , heuristicWeight = 850):
+    #Initialise the open list(priority queue) and closedSet(visited nodes)
+    openList = MinHeap()
+    closedSet = set()
+
+    cameFrom = {}# Dictionary  to track the path
+    exploredEdges = []
+    #Initialising g and f  for every node to be infinity
+    g_score = {node: float('inf') for node in graph.nodes}
+    f_score = {node: float('inf') for node in graph.nodes}
+
+    g_score[startNode] = 0
+
+    #Calculate initial heuristic estimate from start to end node. 
+    initialHeuristicEstimate = HaversineDistance(graph, startNode, endNode)
+
+    f_score[startNode] = g_score[startNode] + initialHeuristicEstimate
+
+    #Insert the start node into the open list with f score
+    openList.Insert((f_score[startNode], startNode))
+    while not openList.IsEmpty():
+        #Get node with the lowest f score
+        _, currentNode = openList.RemoveMinValue()
+
+        if currentNode == endNode:
+            path = GetPathLinkedList(cameFrom[endNode]) #Reconstruct path.
+            lengthOfPath = g_score[endNode]
+            return path,  exploredEdges, lengthOfPath
+        closedSet.add(currentNode)
+
+        #Iterate over neighbours of the current node
+        for neighbourNode in graph.neighbors(currentNode):
+            #Skip neighbour node if already visited
+            if neighbourNode in closedSet:
+                continue
+            #Get length from current node to neighbour node
+            distance = graph[currentNode][neighbourNode][0]['length']
+            provisionalGScore = distance + g_score[currentNode]
+
+            #if provisional g score is less than current g score update the score 
+            if provisionalGScore < g_score[neighbourNode]:
+                #Use memory reference to track the parent node for path reconstruction.
+                cameFrom[neighbourNode] = LinkedListNode(neighbourNode, parent=cameFrom.get(currentNode))
+                g_score[neighbourNode] = provisionalGScore
+                #Increase heuristic by a multiplier for strongr weighting 
+                f_score[neighbourNode] = provisionalGScore + HaversineDistance(graph, neighbourNode, endNode) * heuristicWeight
+
+                #Update new f score for neighbour node
+                openList.Insert((f_score[neighbourNode], neighbourNode))
+
+                #Mark edge as explored
+                exploredEdges.append((currentNode, neighbourNode))
+                
+            
+    #No path found
+    return [],  exploredEdges, None
+
+
+
+
+def Dijkstra(graph: nx.MultiDiGraph, startNode: int, endNode: int):
+    nodesToBeVisited = MinHeap()
+    visitedNodes = set()
+
+    cameFrom = {}
+    exploredEdges = []
+
+    distances = {node: float('inf') for node in graph.nodes} #Initialise the distance to every node as infinity.
+    distances[startNode] = 0
+    nodesToBeVisited.Insert((distances[startNode], startNode)) #Mark start node as first node to be visited
+
+    while not nodesToBeVisited.IsEmpty():
+        currentNodeAndDistance = nodesToBeVisited.RemoveMinValue()
+        currentNode = currentNodeAndDistance[1]
+        if currentNode == endNode:
+            path = GetPathLinkedList(cameFrom[endNode]) #Reconstruct path
+            lengthOfPath = distances[endNode]
+            return path,  exploredEdges, lengthOfPath
+        visitedNodes.add(currentNode) # add node to visited nodes set
+
+        for neighbourNode in graph.neighbors(currentNode): #Explore ever neighbour node.
+            if neighbourNode in visitedNodes: #Skip node if already visited
+                continue
+            currentDistance = distances[currentNode] + graph[currentNode][neighbourNode][0]['length']
+            if currentDistance < distances[neighbourNode]:
+                #Add node to linked list for path reconstruction
+                cameFrom[neighbourNode] = LinkedListNode(neighbourNode, parent=cameFrom.get(currentNode))
+                distances[neighbourNode] = currentDistance
+                neighbourNodeAndDistance = (distances[neighbourNode], neighbourNode)
+                #Mark edge as explored
+                exploredEdges.append((currentNode, neighbourNode))
+                nodesToBeVisited.Insert(neighbourNodeAndDistance)            
+
+    #No path found
+    return [], exploredEdges, None
 
 if __name__ == "__main__":
     start_time = time.time()
