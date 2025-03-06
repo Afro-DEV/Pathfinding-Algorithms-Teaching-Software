826caa8f8d070d244e2bb8bb5a1016d7ff08594a
Ibi <ibitayosalawu@gmail.com>
2025-03-06 20:23:13 +0000

    Initialise Statistics File

---
+++ Statistics.py
@@ -0,0 +1,12 @@
+import csv
+
+class StatisticsManager():
+    def __init__(self):
+        pass
+    def AddEntry():
+        ...
+    def DropAllEntries():
+        ...
+    
+    def PreviewEntries():
+        ...
